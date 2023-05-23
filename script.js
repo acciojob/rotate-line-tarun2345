2594839a063c565bb83d426557c4f76f9a39f009
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-05-23 13:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let line = document.getElementById('line');
+
+let rotation = 0;
+
+setInterval(() => {
+  rotation += 1;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 10);
